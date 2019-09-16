1b430080a559148f667e74ad520754225ff06c42
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2019-09-16 17:11:09 -0300

    first commit

---
+++ src/service-bus-dotnet-management/AppOptions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureServiceBusDotNetManagement
+{
+    using Microsoft.Azure.Management.ServiceBus.Models;
+    using System.Collections.Generic;
+
+    public class AppOptions
+    {
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string SubscriptionId { get; set; }
+        public string DataCenterLocation { get; set; }
+        public SkuName ServiceBusSkuName { get; set; }
+        public SkuTier? ServiceBusSkuTier { get; set; }
+        public string ResourceGr { get; set; }
+        public string BusNSpace { get; set; }
+        public List<string> Queues { get; set; }
+
+    }
+}
